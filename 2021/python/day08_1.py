7aa96bf597646802fa02c212f8276809eee9e837
Scott Halgrim <shalgrim@gmail.com>
2021-12-10 14:49:00 -0800

    cleanup day 8

--- 2021/python/day08_1.py
+++ 2021/python/day08_1.py
@@ -14,9 +14,6 @@ def main(lines):
     num_4s = sum([output_contains_how_many(line, 4) for line in lines])
     num_7s = sum([output_contains_how_many(line, 7) for line in lines])
     num_8s = sum([output_contains_how_many(line, 8) for line in lines])
-    # num_4s = len([line for line in lines if output_contains_how_many(line, 4)])
-    # num_7s = len([line for line in lines if output_contains_how_many(line, 7)])
-    # num_8s = len([line for line in lines if output_contains_how_many(line, 8)])
     print(f'{num_1s=}')
     print(f'{num_4s=}')
     print(f'{num_7s=}')
@@ -26,6 +23,5 @@ def main(lines):
 
 if __name__ == '__main__':
     with open('../data/input08.txt') as f:
-    # with open('../data/test08.txt') as f:
             lines = [line.strip() for line in f.readlines()]
     print(main(lines))
