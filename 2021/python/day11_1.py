42d4673b446226d705aeabee2b932a736a39d8f9
Scott Halgrim <shalgrim@gmail.com>
2021-12-10 14:55:54 -0800

    day 11 prep

---
+++ 2021/python/day11_1.py
@@ -0,0 +1,10 @@
+with open('../data/input11.txt') as f:
+    PUZZLE_INPUT = [line.strip() for line in f.readlines()]
+
+
+def main(lines):
+    pass
+
+
+if __name__ == '__main__':
+    print(main(PUZZLE_INPUT))
---
+++ 2021/python/day11_2.py
@@ -0,0 +1,9 @@
+from day11_1 import PUZZLE_INPUT
+
+
+def main(lines):
+    ...
+
+
+if __name__ == '__main__':
+    print(main(PUZZLE_INPUT))
