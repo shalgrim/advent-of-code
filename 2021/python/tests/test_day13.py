f14d233dd115bd6e12dbbde66a7837af239d379f
Scott Halgrim <shalgrim@gmail.com>
2021-12-12 21:27:46 -0800

    add day 13 test before refactor

---
+++ 2021/python/tests/test_day13.py
@@ -0,0 +1,27 @@
+import textwrap
+
+import pytest
+from day13_1 import main as main1
+from day13_2 import main as main2
+
+
+@pytest.fixture
+def puzzle_input():
+    with open('../../data/input13.txt') as f:
+        return [line.strip() for line in f.readlines()]
+
+
+part_2_answer = '\n'.join(
+    [
+        '###..####.#..#.###..#..#.###..#..#.###.',
+        '#..#.#....#..#.#..#.#..#.#..#.#.#..#..#',
+        '#..#.###..#..#.#..#.#..#.#..#.##...#..#',
+        '###..#....#..#.###..#..#.###..#.#..###.',
+        '#.#..#....#..#.#....#..#.#....#.#..#.#.',
+        '#..#.####..##..#.....##..#....#..#.#..#',
+    ]
+)
+
+
+def test_part2(puzzle_input):
+    assert main2(puzzle_input) == part_2_answer
