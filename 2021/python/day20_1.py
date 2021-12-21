410e05ff11447ebc7f61882a01206b60c247e4e1
Scott Halgrim <shalgrim@gmail.com>
2021-12-20 21:51:01 -0600

    day20 part 1 success

--- 2021/python/day20_1.py
+++ 2021/python/day20_1.py
@@ -1,15 +1,19 @@
-def extend_image(input_image):
+def extend_image(input_image, flipped=False):
+    extra_char = '#' if flipped else '.'
     extended_image = []
-    extended_image.append('.' * (len(input_image[0]) + 4))
-    extended_image.append('.' * (len(input_image[0]) + 4))
+    extended_image.append(extra_char * (len(input_image[0]) + 4))
+    extended_image.append(extra_char * (len(input_image[0]) + 4))
     for line in input_image:
-        extended_image.append('..' + line + '..')
-    extended_image.append('.' * (len(input_image[0]) + 4))
-    extended_image.append('.' * (len(input_image[0]) + 4))
+        if flipped:
+            extended_image.append('##' + line + '##')
+        else:
+            extended_image.append('..' + line + '..')
+    extended_image.append(extra_char * (len(input_image[0]) + 4))
+    extended_image.append(extra_char * (len(input_image[0]) + 4))
     return extended_image
 
 
-def get_binary_representation(y, x, extended_image):
+def get_binary_representation(y, x, extended_image, flipped=False):
     square_rep = []
     for my_y in range(y - 1, y + 2):
         line = []
@@ -20,7 +24,7 @@ def get_binary_representation(y, x, extended_image):
                 or my_y >= len(extended_image)
                 or my_x >= len(extended_image[y])
             ):
-                line.append('.')
+                line.append('#' if flipped else '.')
             else:
                 line.append(extended_image[my_y][my_x])
         square_rep.append(line)
@@ -34,20 +38,19 @@ def get_binary_representation(y, x, extended_image):
     return int(binary_rep, 2)
 
 
-def process_image(image, algorithm):
+def process_image(image, algorithm, flipped=False):
     processed_image = []
     for y, line in enumerate(image):
         processed_line = []
         for x, c in enumerate(line):
-            index = get_binary_representation(y, x, image)
-            print(index)
+            index = get_binary_representation(y, x, image, flipped=flipped)
             processed_line.append(algorithm[index])
         processed_image.append(''.join(processed_line))
 
     return processed_image
 
 
-def main(lines):
+def main_test(lines):
     image_enhancement_algorithm = lines[0]
 
     input_image = lines[2:]
@@ -58,11 +61,30 @@ def main(lines):
     answer = 0
     for line in processed_image:
         answer += len([c for c in line if c == '#'])
+
     return answer
 
 
-if __name__ == '__main__':  # 5908 is too high
-    # with open('../data/test20.txt') as f:
+def main(lines):
+    image_enhancement_algorithm = lines[0]
+
+    input_image = lines[2:]
+    extended_image = extend_image(input_image)
+    processed_image = process_image(extended_image, image_enhancement_algorithm)
+    extended_image = extend_image(processed_image, flipped=True)
+    processed_image = process_image(extended_image, image_enhancement_algorithm, flipped=True)
+    answer = 0
+    for line in processed_image:
+        answer += len([c for c in line if c == '#'])
+    return answer
+
+
+if __name__ == '__main__':
+    with open('../data/test20.txt') as f:
+        lines = [line.strip() for line in f.readlines()]
+
+    print(main_test(lines))
+
     with open('../data/input20.txt') as f:
             lines = [line.strip() for line in f.readlines()]
 
