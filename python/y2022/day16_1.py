de7df3a4401fdde804556796f768cb7e8f7a2aeb
Scott Halgrim <shalgrim@gmail.com>
2022-12-15 20:58:39 -0800

    day 16 stubs

---
+++ python/y2022/day16_1.py
@@ -0,0 +1,7 @@
+def main(lines):
+    pass
+
+if __name__ == '__main__':
+    with open('../../data/2022/test16.txt') as f:
+        lines = [line.strip() for line in f.readlines()]
+    print(main(lines))
