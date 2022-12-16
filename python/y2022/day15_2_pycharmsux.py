19d043e4f13f75910a5ec88c1ba00fdfbf58f863
Scott Halgrim <shalgrim@gmail.com>
2022-12-15 20:48:50 -0800

    day 15 part 2 takes too long

--- python/y2022/day15_2_pycharmsux.py
+++ python/y2022/day15_2_pycharmsux.py
@@ -6,20 +6,22 @@ def main(lines, max_val):
     all_known_beacons = [(s.beacon_x, s.beacon_y) for s in sensors]
     all_impossibles = set()
     for y in range(max_val+1):
+        print(f'{y=}')
         # current issue is that for y == 0 the logic says any x is possible, no impossibles for any sensor, which what?
         # easiest counterexample there is that there's a sensor at 2, 0 that detects a beacon at 2, 10
         # that's a distance of 8. So there should be no possible beacons at anything on row 0 where x >= 10
         impossibles = get_impossibles(sensors, all_known_beacons, y)
         all_impossibles = all_impossibles.union({(x, y) for x in impossibles})
     for x in range(max_val):
+        print(f'{x=}')
         for y in range(max_val):
             if (x, y) not in all_impossibles and (x, y) not in all_known_beacons:
                 return x, y
 
 
 if __name__ == '__main__':
-    with open('../../data/2022/test15.txt') as f:
-    # with open('../../data/2022/input15.txt') as f:
+    # with open('../../data/2022/test15.txt') as f:
+    with open('../../data/2022/input15.txt') as f:
         lines = [line.strip() for line in f.readlines()]
-    print(main(lines, 20))
-    # print(main(lines, 2_000_000))
+    # print(main(lines, 20))
+    print(main(lines, 2_000_000))
