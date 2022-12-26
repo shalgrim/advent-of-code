e951d0cafca914b4efa60395aa6ef31720627401
Scott Halgrim <shalgrim@gmail.com>
2022-12-25 21:25:19 -0800

    day 20 part 2 success

---
+++ python/y2022/day20_2.py
@@ -0,0 +1,35 @@
+from y2022.day20_1 import Number
+
+DECRYPTION_KEY = 811589153
+
+
+def main(lines):
+    raw_nums = [int(line) * DECRYPTION_KEY for line in lines]
+    numbers = [Number(n) for n in raw_nums]
+    numbers_by_original_index = {i: number for i, number in enumerate(numbers)}
+
+    for _ in range(10):
+        for original_index in range(len(numbers_by_original_index)):
+            number_to_move = numbers_by_original_index[original_index]
+            current_index = numbers.index(number_to_move)
+            movement = current_index + number_to_move.value
+            new_index = movement % (len(numbers) - 1)
+            numbers.pop(current_index)
+            if new_index == 0:
+                numbers.append(number_to_move)
+            else:
+                numbers.insert(new_index, number_to_move)
+
+    post_move_raw_numbers = [n.value for n in numbers]
+    zero_index = post_move_raw_numbers.index(0)
+    val1 = post_move_raw_numbers[(zero_index + 1000) % len(post_move_raw_numbers)]
+    val2 = post_move_raw_numbers[(zero_index + 2000) % len(post_move_raw_numbers)]
+    val3 = post_move_raw_numbers[(zero_index + 3000) % len(post_move_raw_numbers)]
+    return val1 + val2 + val3
+
+
+if __name__ == '__main__':
+    with open('../../data/2022/input20.txt') as f:
+        lines = [line.strip() for line in f.readlines()]
+    answer = main(lines)
+    print(answer)
