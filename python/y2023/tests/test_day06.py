207568c3a422ec5bc9567f54652d013a46f5b989
Scott Halgrim <shalgrim@gmail.com>
2023-12-05 20:23:23 -0800

    day 6 prep

---
+++ python/y2023/tests/test_day06.py
@@ -0,0 +1,18 @@
+import pytest
+
+from y2023.day06_1 import main
+from y2023.day06_2 import main as main2
+
+@pytest.fixture
+def test_input():
+    with open("../../../data/2023/test06.txt") as f:
+        return [line.strip() for line in f.readlines()]
+
+
+def test_part_1(test_input):
+    assert main(test_input) == 35
+
+
+def test_part_2(test_input):
+    assert main2(test_input) == 46
+
