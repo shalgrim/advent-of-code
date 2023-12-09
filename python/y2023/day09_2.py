72250e34abdab1f0b6c71a312ad197492a9753d3
Scott Halgrim <shalgrim@gmail.com>
2023-12-08 23:15:21 -0800

    day 9 success

--- python/y2023/day09_2.py
+++ python/y2023/day09_2.py
@@ -1,5 +1,18 @@
+from y2023.day09_1 import produce_sub_line
+
+
+def get_prev_num(nums):
+    if all(n == 0 for n in nums):
+        return 0
+    return nums[0] - get_prev_num(produce_sub_line(nums))
+
+
 def main(lines):
-    pass
+    num_lines = []
+    for line in lines:
+        num_lines.append([int(n) for n in line.split()])
+    return sum(get_prev_num(nums) for nums in num_lines)
+
 
 if __name__ == "__main__":
     with open("../../data/2023/input09.txt") as f:
--- python/y2023/tests/test_day09.py
+++ python/y2023/tests/test_day09.py
@@ -1,5 +1,6 @@
 import pytest
 from y2023.day09_1 import get_next_num, main, produce_sub_line
+from y2023.day09_2 import get_prev_num
 from y2023.day09_2 import main as main2
 
 
@@ -17,9 +18,16 @@ def test_get_next_num():
     assert get_next_num([0, 3, 6, 9, 12, 15]) == 18
 
 
+def test_get_prev_num():
+    assert get_prev_num([2, 2, 2]) == 2
+    assert get_prev_num([0, 2, 4, 6]) == -2
+    assert get_prev_num([3, 3, 5, 9, 15]) == 5
+    assert get_prev_num([10, 13, 16, 21, 30, 45]) == 5
+
+
 def test_part_1(test_input):
     assert main(test_input) == 114
 
 
 def test_part_2(test_input):
-    assert main2(test_input) == 5905
+    assert main2(test_input) == 2
