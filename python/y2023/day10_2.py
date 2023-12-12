aef1314f41869311184673dae4bbac091a9c1740
Scott Halgrim <shalgrim@gmail.com>
2023-12-12 14:24:47 -0800

    day 10 success

--- python/y2023/day10_2.py
+++ python/y2023/day10_2.py
@@ -29,9 +29,7 @@ def generate_clean_map(s_shape, loop, lines, outfile=None):
         outline = ""
         for x, char in enumerate(line):
             if (x, y) in loop:
-                outline += char
-            elif char == "S":
-                outline += s_shape
+                outline += s_shape if char == "S" else char
             else:
                 outline += "."
         outlines.append(outline)
@@ -60,7 +58,58 @@ def print_with_enclosures(map, enclosed_points, enclosfn):
             f.writelines(lines_to_write)
 
 
+def expand_line(line):
+    outline = ""
+    for i, c in enumerate(line[:-1]):
+        if c in "-FL":  # needs to continue to the right
+            outline += f"{c}-"
+        elif (
+            c in "7|J."
+        ):  # would be space to the right of the vertical part or just more space
+            outline += f"{c}."
+    outline += line[-1]
+
+    return outline
+
+
+def touches_o(map, x, y):
+    coords_to_check = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+    try:
+        return any(map[cy][cx] == "O" for cx, cy in coords_to_check)
+    except IndexError:
+        print(x, y)
+        raise
+
+
+def fill_up_with_Os(map):
+    # convert to lists for easier changes
+    outmap = [list(line) for line in map]
+
+    # fill up top and bottom row
+    outmap[0] = ["O" if c == "." else c for c in outmap[0]]
+    outmap[-1] = ["O" if c == "." else c for c in outmap[-1]]
+
+    # fill up left and right column
+    for row in outmap:
+        row[0] = "O" if row[0] == "." else row[0]
+        row[-1] = "O" if row[-1] == "." else row[-1]
+
+    # fill up anything touching O's (until no more changes)
+    changes = True
+    while changes:
+        changes = False
+        for y, row in enumerate(outmap[1:-1], 1):
+            for x, cell in enumerate(row[1:-1], 1):
+                if cell == "." and touches_o(outmap, x, y):
+                    outmap[y][x] = "O"
+                    changes = True
+
+    outmap = ["".join(row) for row in outmap]
+    return outmap
+
+
 def main(lines, outfn=None, enclosfn=None):
+    # Algorithm 1: Use regexes
     s_shape, loop = generate_loop(lines)
     map = generate_clean_map(s_shape, loop, lines, outfn)
     points_of_loop = set(loop)
@@ -81,6 +130,24 @@ def main(lines, outfn=None, enclosfn=None):
         print_with_enclosures(map, enclosed_points, enclosfn)
     return len(enclosed_points)
 
+    # Algorithm 2: Expand space between each row and column then fill it up with O's and I's
+    # expanded_map = [expand_line(line) for line in map]
+    # doubly_expanded_map = []
+    # for line in expanded_map[:-1]:
+    #     doubly_expanded_map.append(line)
+    #     newline = ""
+    #     for c in line[:-1]:
+    #         if c in "|7F":
+    #             newline += "|"
+    #         elif c in "-LJ.":
+    #             newline += "."
+    #     newline += line[-1]
+    # doubly_expanded_map.append(expanded_map[-1])
+    # o_filled_map = fill_up_with_Os(doubly_expanded_map)
+    #
+    # # anything still '.' should be inside
+    # return sum(line.count('.') for line in o_filled_map)
+
 
 if __name__ == "__main__":  # 454 is too high
     sys.setrecursionlimit(20000)
@@ -89,7 +156,7 @@ if __name__ == "__main__":  # 454 is too high
     print(
         main(
             lines,
-            "../../data/2023/output10_2.txt",
-            "../../data/2023/output10_2_enclosed.txt",
+            # "../../data/2023/output10_2.txt",
+            # "../../data/2023/output10_2_enclosed.txt",
         )
     )
