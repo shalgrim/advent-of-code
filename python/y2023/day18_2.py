378a7c39dc3e26e394628495a0cddd846ae13dc4
Scott Halgrim <shalgrim@gmail.com>
2023-12-17 21:00:25 -0800

    day 18 setup

---
+++ python/y2023/day18_2.py
@@ -0,0 +1,8 @@
+def main(lines):
+    pass
+
+
+if __name__ == "__main__":
+    with open("../../data/2023/input18.txt") as f:
+        lines = [line.strip() for line in f.readlines()]
+    print(main(lines))
