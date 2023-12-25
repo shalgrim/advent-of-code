2249e935445ed373753728d06c721dfe1dae067b
Scott Halgrim <shalgrim@gmail.com>
2023-12-24 17:57:30 -0800

    make it so i can run from command line

---
+++ python/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='advent_of_code',          # Required
+    version='0.0.1',           # Required
+    packages=find_packages(),  # Required
+)
+
