253bd8fce3cefee303fe5902d51b7891dffd4db3
Scott Halgrim <scott.halgrim@zapier.com>
2024-06-03 22:37:12 -0700

    is fast now

--- python/y2022/day16_1.py
+++ python/y2022/day16_1.py
@@ -24,15 +24,6 @@ def build_valves(lines):
     return {v.name: v for v in valves}
 
 
-def calc_total_pressure_release(state_history, valves):
-    final_state = state_history.states[-1]
-    opened_at_tracker = final_state.opened_valves
-    answer = 0
-    for valve_id, time_opened in opened_at_tracker.items():
-        answer += (30 - time_opened) * valves[valve_id].flow_rate
-    return answer
-
-
 class Valve:
     def __init__(self, name, flow_rate, destinations):
         self.name = name
@@ -51,9 +42,12 @@ class State:
         return hash((self.current_valve_id, tuple(self.opened_valves.items())))
 
 
-class StateHistory:
-    def __init__(self, states: list[State]):
-        self.states = states
+def calc_total_pressure_release(state: State, valves):
+    opened_at_tracker = state.opened_valves
+    answer = 0
+    for valve_id, time_opened in opened_at_tracker.items():
+        answer += (30 - time_opened) * valves[valve_id].flow_rate
+    return answer
 
 
 def all_flow_positive_valves_open(
@@ -70,36 +64,24 @@ def does_not_contain(states: list[State], state: State) -> bool:
 
 
 def generate_new_moves(
-    state_history: StateHistory,
+    state: State,
     t: int,
     valves: dict[str, Valve],
 ) -> list[State]:
-    states = state_history.states
-    most_recent_state = states[-1]
+    if all_flow_positive_valves_open(state, valves):
+        return []
 
-    if all_flow_positive_valves_open(most_recent_state, valves):
-        return [
-            State(
-                most_recent_state.current_valve_id,
-                copy(most_recent_state.opened_valves),
-            )
-        ]
-
-    current_valve_id = most_recent_state.current_valve_id
+    current_valve_id = state.current_valve_id
     current_valve = valves[current_valve_id]
     possible_next_moves = copy(current_valve.destinations)
     possible_next_states = []
 
     for move in possible_next_moves:
-        new_state = State(move, copy(most_recent_state.opened_valves))
-        if new_state not in states:
-            possible_next_states.append(new_state)
+        new_state = State(move, copy(state.opened_valves))
+        possible_next_states.append(new_state)
 
-    if (
-        current_valve_id not in most_recent_state.opened_valves
-        and current_valve.flow_rate
-    ):
-        new_opened_valves = copy(most_recent_state.opened_valves)
+    if current_valve_id not in state.opened_valves and current_valve.flow_rate:
+        new_opened_valves = copy(state.opened_valves)
         new_opened_valves[current_valve_id] = t
         new_state = State(current_valve_id, new_opened_valves)
         possible_next_states.append(new_state)
@@ -107,45 +89,26 @@ def generate_new_moves(
     return possible_next_states
 
 
-def uniquify(state_histories: List[StateHistory]) -> List[StateHistory]:
-    """From a list of StateHistories, returns only one of each final state"""
-    uniques = []
-
-    # TODO: Adding this sped things way up, but I may still need to track all seen states and just never revisit
-    # regardless of t. To do that I will need to create a set of seen states in `main` and pass it to
-    # `generate_new_moves` and `uniquify` and in doing so I think I can get rid of the concept of state histories
-    # altogether
-    finals = set()
-
-    for sh in state_histories:
-        if sh.states[-1] not in finals:
-            finals.add(sh.states[-1])
-            uniques.append(sh)
-
-    return uniques
-
-
 def main(lines):
     """BFS that checks all possibilities then finds max"""
     valves = build_valves(lines)
     initial_state = State("AA", {})
-    # seen_states = set()
-    # seen_states.add(initial_state)
-    state_histories = [StateHistory(initial_state)]
+    seen_states = set()
+    seen_states.add(initial_state)
+    current_states = set()
+    current_states.add(initial_state)
 
     for t in range(1, 31):
         print(f"== Minute {t} ==")
-        new_state_histories = []
-        for history in state_histories:
-            for new_state in generate_new_moves(history, t, valves):
-                new_state_list = copy(history.states)
-                new_state_list.append(new_state)
-                new_state_histories.append(StateHistory(new_state_list))
-        state_histories = uniquify(new_state_histories)
+        new_current_states = set()
+        for state in current_states:
+            for new_state in generate_new_moves(state, t, valves):
+                if new_state not in seen_states:
+                    new_current_states.add(new_state)
+        seen_states.update(new_current_states)
+        current_states = new_current_states
 
-    totals = [
-        calc_total_pressure_release(history, valves) for history in state_histories
-    ]
+    totals = [calc_total_pressure_release(state, valves) for state in seen_states]
     return max(totals)
 
 
--- python/y2022/tests/test_day16.py
+++ python/y2022/tests/test_day16.py
@@ -2,7 +2,6 @@ import pytest
 from y2022.day16_1 import (
     main as main1,
     calc_total_pressure_release,
-    StateHistory,
     State,
     build_valves,
 )
@@ -16,15 +15,15 @@ def tst_input():
 
 @pytest.fixture
 def puzzle_input():
-    with open("../../../data/2022/test16.txt") as f:
+    with open("../../../data/2022/input16.txt") as f:
         return [line.strip() for line in f.readlines()]
 
 
 def test_calc_total_pressure_release(tst_input):
     valves = build_valves(tst_input)
     tracker = {"DD": 2, "BB": 5, "JJ": 9, "HH": 17, "EE": 21, "CC": 24}
-    state_history = StateHistory([State("CC", tracker)])
-    assert calc_total_pressure_release(state_history, valves) == 1651
+    state = State("CC", tracker)
+    assert calc_total_pressure_release(state, valves) == 1651
 
 
 def test_main1(tst_input, puzzle_input):
