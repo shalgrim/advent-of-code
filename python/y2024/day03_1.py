b5321efd37b96f537031f62a64d8ef9d2c14eddf
Scott Halgrim <scott.halgrim@zapier.com>
2024-12-03 11:50:14 -0800

    day 3 ugly solve

--- python/y2024/day03_1.py
+++ python/y2024/day03_1.py
@@ -1,13 +1,23 @@
+import re
+
 from aoc.io import this_year_day
 
+pattern = re.compile(r"mul\((\d+),(\d+)\)")
 
-def main(lines):
-    ...
+
+def multiply(s1, s2):
+    return int(s1) * int(s2)
+
+
+def main(text):
+    groupings = pattern.findall(text)
+    products = [multiply(*group) for group in groupings]
+    return sum(products)
 
 
 if __name__ == "__main__":
     year, day = this_year_day(pad_day=True)
     with open(f"../../data/{year}/input{day}.txt") as f:
         # with open(f"../../data/{year}/test{day}.txt") as f:
-        lines = [line.strip() for line in f.readlines()]
-    print(main(lines))
+        text = f.read()
+    print(main(text))
--- python/y2024/day03_2.py
+++ python/y2024/day03_2.py
@@ -1,13 +1,30 @@
+import re
+
 from aoc.io import this_year_day
+from y2024.day03_1 import multiply
+
+pattern = re.compile(r"(?:mul\((\d+),(\d+)\))|(do\(\))|(don't\(\))")
 
 
-def main(lines):
-    ...
+def main(text):
+    groupings = pattern.findall(text)
+    valid_groupings = []
+    valid = True
+    for grouping in groupings:
+        if not valid and grouping[2]:
+            valid = True
+        elif grouping[3]:
+            valid = False
+        elif valid and grouping[0]:
+            valid_groupings.append(grouping[:2])
+
+    products = [multiply(*group) for group in valid_groupings]
+    return sum(products)
 
 
 if __name__ == "__main__":
     year, day = this_year_day(pad_day=True)
     with open(f"../../data/{year}/input{day}.txt") as f:
-        # with open(f"../../data/{year}/test{day}.txt") as f:
-        lines = [line.strip() for line in f.readlines()]
-    print(main(lines))
+        # with open(f"../../data/{year}/test{day}_2.txt") as f:
+        text = f.read()
+    print(main(text))
