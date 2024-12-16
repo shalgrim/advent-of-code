f7caef4791ef0577f942032acd5f79ee7a81ad0a
Scott Halgrim <scott.halgrim@zapier.com>
2024-12-15 19:54:21 -0800

    test before refactoring

---
+++ python/y2024/tests/test_day15.py
@@ -0,0 +1,13 @@
+import pytest
+
+from y2024.day15_2 import main
+
+
+@pytest.fixture
+def file15_large_lines():
+    with open(f"../../../data/2024/test15_large.txt") as f:
+        return [line.strip() for line in f.readlines()]
+
+
+def test_part_2(file15_large_lines):
+    assert main(file15_large_lines) == 9021
