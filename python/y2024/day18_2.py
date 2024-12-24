cd45afcfb75522b8ea49221d0728f49d37bc718e
Scott Halgrim <scott.halgrim@zapier.com>
2024-12-24 11:10:24 -0800

    2024.18.2 manual bisection

---
+++ python/y2024/day18_2.py
@@ -0,0 +1,20 @@
+import math
+
+from aoc.io import this_year_day
+from y2024.day18_1 import main
+
+# def main(lines, grid_size, num_to_fall):
+#     ...
+
+if __name__ == "__main__":
+    # testing = True
+    testing = False
+    filetype = "test" if testing else "input"
+    year, day = this_year_day(pad_day=True)
+    with open(f"../../data/{year}/{filetype}{day}.txt") as f:
+        lines = [line.strip() for line in f.readlines()]
+    grid_size = 7 if testing else 71
+    # OK 3037
+    # Not OK 3038
+    num_to_fall = 21 if testing else 3037
+    print(main(lines, grid_size, num_to_fall))
---
+++ python/y2024/tests/test_day18.py
@@ -0,0 +1,13 @@
+import pytest
+
+from y2024.day18_1 import main
+
+
+@pytest.fixture
+def test_file_18_lines():
+    with open(f"../../../data/2024/test18.txt") as f:
+        return [line.strip() for line in f.readlines()]
+
+
+def test_part_1(test_file_18_lines):
+    assert main(test_file_18_lines, 6, 12) == 22
