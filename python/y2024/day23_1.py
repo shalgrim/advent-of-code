226dd862c6e21d2b5e7c5da8b40a15f839441f2e
Scott Halgrim <scott.halgrim@zapier.com>
2024-12-23 22:18:11 -0800

    y2024.23.1 success

--- python/y2024/day23_1.py
+++ python/y2024/day23_1.py
@@ -1,3 +1,4 @@
+import itertools
 from collections import defaultdict
 
 from aoc.io import this_year_day
@@ -16,50 +17,25 @@ def main(lines):
     connections = make_connections(lines)
     computers = set(connections.keys())
 
-    # remove those we know are connected to three others
-    possibilities = {c for c in computers if len(connections[c]) < 3}
-
-    # now for each one...something
     networks_of_three = set()
-    for p in possibilities:
-        if any(p in t for t in networks_of_three):
-            # We've already put this one in a network of three
-            continue
+    for a, a_conns in connections.items():
+        for b, c in itertools.combinations(a_conns, 2):
+            if c in connections[b]:
+                networks_of_three.add(tuple(sorted([a, b, c])))
 
-        direct_connections = connections[p]
+    t_networks = {network for network in networks_of_three if "t" in "".join(network)}
+    starts_with_t_networks = {
+        network
+        for network in t_networks
+        if ",".join(network).startswith("t") or ",t" in ",".join(network)
+    }
 
-        if len(direct_connections) == 1:
-            # It only connects to one other
-            # So check if that one connects to two others (one will be p)
-            # and that that third item only connects to the second item
-            second_item = direct_connections[0]
-            secondary_connections = connections[second_item]
-            if len(secondary_connections) != 2:
-                continue
-            third_item = [c for c in secondary_connections if c != p][0]
-            assert len(connections[third_item]) == 1
-            networks_of_three.add((p, second_item, third_item))
-        elif len(direct_connections) == 2:
-            # Check that those two connections don't connect to anything else
-            second_item, third_item = direct_connections
-            potential_tuple = (p, second_item, third_item)
-            if set(connections[second_item]).difference(set(potential_tuple)):
-                continue
-            if set(connections[third_item]).difference(set(potential_tuple)):
-                continue
-            networks_of_three.add(potential_tuple)
-        else:
-            raise ValueError("Shouldn't be here")
-
-    # Now I have all networks_of_three
-    # TODO: do this with a generator expression like you learned the other day
-    stringified = [",".join(t) for t in networks_of_three]
-    return len([s for s in stringified if "t" in s])
+    return len(starts_with_t_networks)
 
 
 if __name__ == "__main__":
-    testing = True
-    # testing = False
+    # testing = True
+    testing = False
     filetype = "test" if testing else "input"
     year, day = this_year_day(pad_day=True)
     with open(f"../../data/{year}/{filetype}{day}.txt") as f:
