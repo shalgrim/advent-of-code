11e35f8434252159b985340948767a13a82849b3
Scott Halgrim <scott.halgrim@zapier.com>
2024-12-25 17:26:49 -0800

    2024.16.1 finally works

--- python/y2024/day16_1.py
+++ python/y2024/day16_1.py
@@ -1,8 +1,6 @@
-import math
-import sys
-from dataclasses import dataclass
 from enum import IntEnum
-from typing import List
+
+import math
 
 
 class Direction(IntEnum):
@@ -12,142 +10,100 @@ class Direction(IntEnum):
     N = 3
 
 
-def manhattan_distance(p1, p2):
-    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+class Node:
+    def __init__(self, x: int, y: int, facing: Direction):
+        self.x = x
+        self.y = y
+        self.facing = facing
+        self.distance = math.inf
+        self.visited = False
+
+    def __str__(self):
+        return f"{self.x},{self.y},{self.facing.name}"
 
 
-@dataclass
-class State:
-    x: int
-    y: int
-    dir: Direction
-    has_turned: bool
-
-
-def score_states(states: List[State]) -> int:
-    answer = 0
-    for state in states[1:]:
-        if state.has_turned:
-            answer += 1000
-        else:
-            answer += 1
-    return answer
-
-
-class Map:
-    def __init__(self, lines):
-        self.walls = set()
-        self.best_score = math.inf
-        for y, line in enumerate(lines):
-            for x, char in enumerate(line):
-                match char:
-                    case "#":
-                        self.walls.add((x, y))
-                    case "S":
-                        start_x, start_y = x, y
-                    case "E":
-                        self.end = x, y
-
-        self.states = [State(start_x, start_y, Direction.E, False)]
-        self.rightest = start_x
-        self.highest = start_y
-        self.closest = manhattan_distance((start_x, start_y), self.end)
-        self.rightest_1 = -1
-        self.highest_139 = math.inf
-
-    def get_possible_moves(self):
-        moves = []
-        current_state = self.states[-1]
-        me = current_state.x, current_state.y
-        match current_state.dir:
-            case Direction.E:
-                facing_block = me[0] + 1, me[1]
-                ccw_block = me[0], me[1] - 1
-                cw_block = me[0], me[1] + 1
-            case Direction.S:
-                facing_block = me[0], me[1] + 1
-                ccw_block = me[0] + 1, me[1]
-                cw_block = me[0] - 1, me[1]
-            case Direction.W:
-                facing_block = me[0] - 1, me[1]
-                cw_block = me[0], me[1] - 1
-                ccw_block = me[0], me[1] + 1
-            case Direction.N:
-                facing_block = me[0], me[1] - 1
-                ccw_block = me[0] - 1, me[1]
-                cw_block = me[0] + 1, me[1]
-            case _:
-                raise ValueError(f"Unexpected Direction {current_state.dir}")
-        if facing_block not in self.walls and all(
-            facing_block != (state.x, state.y) for state in self.states
-        ):
-            moves.append(
-                State(facing_block[0], facing_block[1], current_state.dir, False)
-            )
-        if not current_state.has_turned and cw_block not in self.walls:
-            new_direction = Direction((current_state.dir + 1) % 4)
-            moves.append(State(current_state.x, current_state.y, new_direction, True))
-        if not current_state.has_turned and ccw_block not in self.walls:
-            new_direction = Direction((current_state.dir - 1) % 4)
-            moves.append(State(current_state.x, current_state.y, new_direction, True))
-
-        return moves
-
-    def find_best_route(self, move=None):
-        if move is not None:
-            self.states.append(move)
-        current_state = self.states[-1]
-        me = current_state.x, current_state.y
-        if current_state.x > self.rightest:
-            print(f"new rightest: {me=}")
-            self.rightest = current_state.x
-        if current_state.y < self.highest:
-            print(f"new highest: {me=}")
-            self.highest = current_state.y
-        if current_state.y == 1 and current_state.x > self.rightest_1:
-            print(f"new rightest_1: {me=}")
-            self.rightest_1 = current_state.x
-        if current_state.x == 139 and current_state.y < self.highest_139:
-            print(f"new highest_139: {me=}")
-            self.highest_139 = current_state.y
-
-        distance = manhattan_distance(me, self.end)
-        if distance < self.closest:
-            print(f"new closest: {distance=} {me=}")
-            self.closest = distance
-
-        current_score = score_states(self.states)
-
-        # base case
-        if me == self.end:
-            self.states.pop()
-            print(f"Found path of {current_score=}")
-            # sys.exit()
-            return current_score
-
-        # abort case
-        if current_score > self.best_score:
-            self.states.pop()
-            return current_score
-
-        possible_moves = self.get_possible_moves()
-
-        # another abort case
-        if not possible_moves:
-            self.states.pop()
-            return math.inf
-
-        for move in possible_moves:
-            score = self.find_best_route(move)
-            self.best_score = min(score, self.best_score)
-
-        self.states.pop()
-        return self.best_score
+def create_nodes(lines):
+    nodes = []
+    for y, line in enumerate(lines):
+        for x, char in enumerate(line):
+            if char == "#":
+                continue
+            elif char == "E":
+                for i in range(4):
+                    nodes.append(Node(x, y, Direction(i)))
+                target_coords = x, y
+            elif char == ".":
+                for i in range(4):
+                    nodes.append(Node(x, y, Direction(i)))
+            elif char == "S":
+                start_node = Node(x, y, Direction.E)
+                start_node.distance = 0
+                nodes.append(start_node)
+                for i in range(1, 4):
+                    nodes.append(Node(x, y, Direction(i)))
+            else:
+                raise ValueError("Shouldn't be here")
+    return nodes, target_coords
 
 
 def main(lines):
-    map = Map(lines)
-    return map.find_best_route()
+    all_nodes, target_coords = create_nodes(lines)
+    target_keys = {(target_coords[0], target_coords[1], Direction(i)) for i in range(4)}
+    unvisited_nodes = {(node.x, node.y, node.facing): node for node in all_nodes}
+    min_distance = min(node.distance for node in unvisited_nodes.values())
+    current_node = [
+        node for node in unvisited_nodes.values() if node.distance == min_distance
+    ][0]
+
+    # This is probably going to run too long tho...
+    while target_keys.intersection(set(unvisited_nodes.keys())):
+        if len(unvisited_nodes) % 1000 == 0:
+            print(f"{len(unvisited_nodes)=}")
+        if current_node.facing == Direction.E:
+            move_coords = (current_node.x + 1, current_node.y)
+            turnable_directions = [Direction.N, Direction.S]
+        elif current_node.facing == Direction.S:
+            move_coords = (current_node.x, current_node.y + 1)
+            turnable_directions = [Direction.E, Direction.W]
+        elif current_node.facing == Direction.W:
+            move_coords = (current_node.x - 1, current_node.y)
+            turnable_directions = [Direction.N, Direction.S]
+        elif current_node.facing == Direction.N:
+            move_coords = (current_node.x, current_node.y - 1)
+            turnable_directions = [Direction.E, Direction.W]
+        else:
+            raise ValueError("Should not be here")
+
+        movable_node = unvisited_nodes.get(
+            (move_coords[0], move_coords[1], current_node.facing)
+        )
+        if movable_node is not None:
+            movable_node.distance = min(
+                movable_node.distance, current_node.distance + 1
+            )
+
+        for direction in turnable_directions:
+            turnable_node = unvisited_nodes.get(
+                (current_node.x, current_node.y, direction)
+            )
+            if turnable_node is not None:
+                turnable_node.distance = min(
+                    turnable_node.distance, current_node.distance + 1000
+                )
+
+        # remove current node from unvisited
+        del unvisited_nodes[(current_node.x, current_node.y, current_node.facing)]
+
+        # get new current_node
+        if not unvisited_nodes:
+            break
+        min_distance = min(node.distance for node in unvisited_nodes.values())
+        current_node = [
+            node for node in unvisited_nodes.values() if node.distance == min_distance
+        ][0]
+
+    target_nodes = [node for node in all_nodes if (node.x, node.y) == target_coords]
+    return min(node.distance for node in target_nodes)
 
 
 if __name__ == "__main__":
@@ -161,9 +117,9 @@ if __name__ == "__main__":
     print(main(lines))
 
     # sys.setrecursionlimit(10000)
-    # with open(f"../../data/2024/input16.txt") as f:
-    #     lines = [line.strip() for line in f.readlines()]
-    # print(main(lines))
+    with open(f"../../data/2024/input16.txt") as f:
+        lines = [line.strip() for line in f.readlines()]
+    print(main(lines))
     # year, day = this_year_day(pad_day=True)
     # # testing = False
     # testing = True
--- python/y2024/tests/test_day16.py
+++ python/y2024/tests/test_day16.py
@@ -4,10 +4,22 @@ from y2024.day16_1 import main
 
 
 @pytest.fixture
-def file16_lines():
-    with open(f"../../../data/2024/test16.txt") as f:
+def test_file16_lines1():
+    with open(f"../../../data/2024/test16_1.txt") as f:
         return [line.strip() for line in f.readlines()]
 
 
+@pytest.fixture
+def test_file16_lines2():
+    with open(f"../../../data/2024/test16_2.txt") as f:
+        return [line.strip() for line in f.readlines()]
+
+
+def test_part1(test_file16_lines1, test_file16_lines2):
+    assert main(test_file16_lines1) == 7036
+    assert main(test_file16_lines2) == 11048
+
+
+@pytest.mark.skip(reason="whydoievenhavethis")
 def test_part_2(file16_lines):
     assert main(file16_lines) == -1
