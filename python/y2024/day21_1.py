70266894efbe04f35fd4c82c822d29dc110dfe12
Scott Halgrim <scott.halgrim@zapier.com>
2024-12-27 22:43:55 -0800

    2024.21.1 works

--- python/y2024/day21_1.py
+++ python/y2024/day21_1.py
@@ -1,8 +1,12 @@
 import itertools
 import math
+import re
 from copy import copy
 from dataclasses import dataclass
+from functools import cache
 from typing import Set
+
+from aoc.io import this_year_day
 
 NUMERIC_PAD = {
     (0, 0): "7",
@@ -19,6 +23,9 @@ NUMERIC_PAD = {
 }
 
 REVERSE_NUMERIC_PAD = {v: k for k, v in NUMERIC_PAD.items()}
+
+DIRECTIONAL_PAD = {(1, 0): "^", (2, 0): "A", (0, 1): "<", (1, 1): "v", (2, 1): ">"}
+REVERSE_DIRECTIONAL_PAD = {v: k for k, v in DIRECTIONAL_PAD.items()}
 
 
 @dataclass
@@ -66,12 +73,67 @@ def find_all_shortest_paths_for_code(code):
     return paths
 
 
+def find_all_shortest_paths_for_directional_code(code):
+    paths = find_all_shortest_paths_directional_pad_plus_press("A", code[0])
+
+    for i in range(len(code) - 1):
+        new_paths = find_all_shortest_paths_directional_pad_plus_press(
+            code[i], code[i + 1]
+        )
+        paths = {"".join(t) for t in itertools.product(paths, new_paths)}
+
+    return paths
+
+
+@cache
 def find_all_shortest_paths_numeric_pad_plus_press(frum, to):
     return {f"{path}A" for path in find_all_shortest_paths_numeric_pad(frum, to)}
 
 
+@cache
+def find_all_shortest_paths_directional_pad_plus_press(frum, to):
+    return {f"{path}A" for path in find_all_shortest_paths_directional_pad(frum, to)}
+
+
+@cache
+def find_all_shortest_paths_directional_pad(frum, to):
+    """Maybe I can just reuse the function below but send in a reverse pad argument?"""
+    if frum == to:
+        return {""}
+    all_nodes = {
+        (v[0], v[1]): Node(v[0], v[1], k) for k, v in REVERSE_DIRECTIONAL_PAD.items()
+    }
+    all_nodes[REVERSE_DIRECTIONAL_PAD[frum]].distance = 0
+    unvisited = copy(all_nodes)
+    while REVERSE_DIRECTIONAL_PAD[to] in unvisited:
+        min_distance = min(node.distance for node in unvisited.values())
+        current_node = [
+            node for node in unvisited.values() if node.distance == min_distance
+        ][0]
+        neighbors = get_neighbors(current_node, unvisited)
+
+        for neighbor in neighbors:
+            direction = directional_character(
+                (current_node.x, current_node.y), (neighbor.x, neighbor.y)
+            )
+            if current_node.distance + 1 < neighbor.distance:
+                neighbor.distance = current_node.distance + 1
+                neighbor.paths = (
+                    {path + direction for path in current_node.paths}
+                    if current_node.paths
+                    else {direction}
+                )
+            elif current_node.distance + 1 == neighbor.distance:
+                neighbor.paths.update({path + direction for path in current_node.paths})
+
+        del unvisited[(current_node.x, current_node.y)]
+
+    return all_nodes[REVERSE_DIRECTIONAL_PAD[to]].paths
+
+
+@cache
 def find_all_shortest_paths_numeric_pad(frum, to):
-    """Now to figure out how to track the actual paths"""
+    """This seems like major overkill"""
     all_nodes = {
         (v[0], v[1]): Node(v[0], v[1], k) for k, v in REVERSE_NUMERIC_PAD.items()
     }
@@ -135,3 +197,48 @@ def find_shortest_numeric_pad(frum, to):
         del unvisited[(current_node.x, current_node.y)]
 
     return all_nodes[REVERSE_NUMERIC_PAD[to]].distance
+
+
+def find_all_shortest_paths_for_second_robot(code, num_iterations=1):
+    first_robot_sez = find_all_shortest_paths_for_code(code)
+    these_are_the_codes_to_iterate_on = first_robot_sez
+
+    for _ in range(num_iterations):
+        answer = set()
+        for path in these_are_the_codes_to_iterate_on:
+            answer.update(find_all_shortest_paths_for_directional_code(path))
+        these_are_the_codes_to_iterate_on = answer
+
+    shortest_distance = min(len(a) for a in answer)
+    answer = {s for s in answer if len(s) == shortest_distance}
+    return answer
+
+
+def find_shortest_length_third_robot(code):
+    """To consider: Is there a possibility that by getting rid of longer ones from second robot I'm missing actual shortest for third?"""
+    paths = find_all_shortest_paths_for_second_robot(code, 2)
+    shortest_distance = min(len(a) for a in paths)
+    return shortest_distance
+
+
+def numericize(code):
+    m = re.search(r"\d+", code)
+    return int(m.group())
+
+
+def complexity(code):
+    return find_shortest_length_third_robot(code) * numericize(code)
+
+
+def main(lines):
+    return sum(complexity(line) for line in lines)
+
+
+if __name__ == "__main__":
+    # testing = True
+    testing = False
+    filetype = "test" if testing else "input"
+    year, day = this_year_day(pad_day=True)
+    with open(f"../../data/{year}/{filetype}{day}.txt") as f:
+        lines = [line.strip() for line in f.readlines()]
+    print(main(lines))
--- python/y2024/tests/test_day21.py
+++ python/y2024/tests/test_day21.py
@@ -5,10 +5,25 @@ from y2024.day21_1 import (
     find_shortest_path_numeric_pad,
     find_all_shortest_paths_numeric_pad_plus_press,
     find_all_shortest_paths_for_code,
+    find_all_shortest_paths_directional_pad,
+    find_all_shortest_paths_for_directional_code,
+    find_all_shortest_paths_for_second_robot,
+    find_shortest_length_third_robot,
+    numericize,
 )
 
 
-# @pytest.mark.skip(reason="not ready yet")
+def test_robot_find_shortest_numeric_pad():
+    assert find_shortest_numeric_pad("A", "0") == 1
+    assert find_shortest_numeric_pad("0", "2") == 1
+    assert find_shortest_numeric_pad("2", "9") == 3
+    assert find_shortest_numeric_pad("9", "A") == 3
+
+
+def test_find_shortest_path_numeric_pad():
+    assert find_shortest_path_numeric_pad("029A") == 12
+
+
 def test_find_all_shortest_paths_numeric_pad():
     assert find_all_shortest_paths_numeric_pad("A", "0") == {"<"}
     assert find_all_shortest_paths_numeric_pad("0", "2") == {"^"}
@@ -29,12 +44,32 @@ def test_find_all_shortest_paths_numeric_pad():
     }
 
 
-def test_robot_find_shortest_numeric_pad():
-    assert find_shortest_numeric_pad("A", "0") == 1
-    assert find_shortest_numeric_pad("0", "2") == 1
-    assert find_shortest_numeric_pad("2", "9") == 3
-    assert find_shortest_numeric_pad("9", "A") == 3
+def test_find_all_shortest_paths_directional_pad():
+    assert find_all_shortest_paths_directional_pad("A", "<") == {"v<<", "<v<"}
+    foo = find_all_shortest_paths_for_directional_code("<A^A>^^AvvvA")
+    bar = find_all_shortest_paths_for_directional_code("<A^A^>^AvvvA")
+    baz = find_all_shortest_paths_for_directional_code("<A^A^^>AvvvA")
+    allofem = foo.union(bar).union(baz)
+    assert "v<<A>>^A<A>AvA<^AA>A<vAAA>^A" in allofem
 
 
-def test_find_shortest_path_numeric_pad():
-    assert find_shortest_path_numeric_pad("029A") == 12
+def test_find_all_shortest_paths_for_second_robot():
+    assert "v<<A>>^A<A>AvA<^AA>A<vAAA>^A" in find_all_shortest_paths_for_second_robot(
+        "029A"
+    )
+
+
+def test_find_shortest_length_third_robot():
+    assert find_shortest_length_third_robot("029A") == 68
+    assert find_shortest_length_third_robot("980A") == 60
+    assert find_shortest_length_third_robot("179A") == 68
+    assert find_shortest_length_third_robot("456A") == 64
+    assert find_shortest_length_third_robot("379A") == 64
+
+
+def test_numericize():
+    assert numericize("029A") == 29
+    assert numericize("980A") == 980
+    assert numericize("179A") == 179
+    assert numericize("456A") == 456
+    assert numericize("379A") == 379
