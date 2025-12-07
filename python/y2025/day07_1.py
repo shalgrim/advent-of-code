87853adc05683658c86c6de3d278f730deee8719
Scott Halgrim <shalgrim@gmail.com>
2025-12-06 21:21:45 -0800

    avoid extra processing; 7.01 complete

--- python/y2025/day07_1.py
+++ python/y2025/day07_1.py
@@ -23,10 +23,12 @@ def get_splits(beam_location, tachyons_by_x, split_points=None):
     beam_x, beam_y = beam_location
     # base-case: no more splits
     if beam_y > max(tachyons_by_x[beam_x], default=0):
-        return 0
+        return set()
 
     # find where it splits
     tachyon_y = min([tach for tach in tachyons_by_x[beam_x] if tach >= beam_y])
+    if (beam_x, tachyon_y) in split_points:
+        return set()
     split_points.add((beam_x, tachyon_y))
     get_splits((beam_x - 1, tachyon_y), tachyons_by_x, split_points)
     get_splits((beam_x + 1, tachyon_y), tachyons_by_x, split_points)
