b7fa0e615a6d7248979d89fdc60d850219012535
Scott Halgrim <shalgrim@gmail.com>
2025-12-06 21:20:26 -0800

    day 7 part 1 test passes

--- python/y2025/day07_1.py
+++ python/y2025/day07_1.py
@@ -18,7 +18,8 @@ def process_input(lines: list[str]) -> tuple:
     return beam_location, tachyons_by_x
 
 
-def get_num_splits(beam_location, tachyons_by_x):
+def get_splits(beam_location, tachyons_by_x, split_points=None):
+    split_points = split_points or set()
     beam_x, beam_y = beam_location
     # base-case: no more splits
     if beam_y > max(tachyons_by_x[beam_x], default=0):
@@ -26,16 +27,15 @@ def get_num_splits(beam_location, tachyons_by_x):
 
     # find where it splits
     tachyon_y = min([tach for tach in tachyons_by_x[beam_x] if tach >= beam_y])
-    return (
-        1
-        + get_num_splits((beam_x - 1, tachyon_y), tachyons_by_x)
-        + get_num_splits((beam_x + 1, tachyon_y), tachyons_by_x)
-    )
+    split_points.add((beam_x, tachyon_y))
+    get_splits((beam_x - 1, tachyon_y), tachyons_by_x, split_points)
+    get_splits((beam_x + 1, tachyon_y), tachyons_by_x, split_points)
+    return split_points
 
 
 def main(lines: list[str]) -> int:
     beam_location, tachyons_by_x = process_input(lines)
-    return get_num_splits(beam_location, tachyons_by_x)
+    return len(get_splits(beam_location, tachyons_by_x))
 
 
 if __name__ == "__main__":
