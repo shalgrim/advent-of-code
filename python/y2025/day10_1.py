688239327162edfba498631aeaa85590d3d582a8
Scott Halgrim <shalgrim@gmail.com>
2025-12-09 21:52:16 -0800

    10.2 brute force won’t work

--- python/y2025/day10_1.py
+++ python/y2025/day10_1.py
@@ -3,12 +3,17 @@ import itertools
 from coding_puzzle_tools import read_input
 
 
+class JoltageError(Exception):
+    pass
+
+
 class Machine:
     def __init__(self, line):
         self._extract_lights(line.split()[0])
         self._extract_buttons(line.split()[1:-1])
-        # ignore joltages for now
+        self._extract_joltages(line.split()[-1])
         self._reset()
+        self._joltage_reset()
 
     def _extract_lights(self, brackets: str):
         self.total_lights = len(brackets) - 2
@@ -26,18 +31,36 @@ class Machine:
             numbers = raw_button[1:-1].split(",")
             self.buttons.append([int(n) for n in numbers])
 
+    def _extract_joltages(self, raw_joltage_data):
+        self.required_joltages = [int(n) for n in raw_joltage_data[1:-1].split(",")]
+
     def push_button(self, index: int) -> None:
         for light_to_switch in self.buttons[index]:
             self.lights[light_to_switch] = not self.lights[light_to_switch]
 
+    def push_joltage_button(self, index: int):
+        for joltage_to_increase in self.buttons[index]:
+            self.joltages[joltage_to_increase] += 1
+
     def check(self) -> bool:
         return self.lights == self.answer
+
+    def joltage_check(self) -> bool:
+        if any(j > r for j, r in zip(self.joltages, self.required_joltages)):
+            raise JoltageError
+        return self.joltages == self.required_joltages
 
     def _reset(self):
         self.lights = [False for _ in range(self.total_lights)]
 
     def reset(self):
         self._reset()
+
+    def _joltage_reset(self):
+        self.joltages = [0 for _ in range(len(self.required_joltages))]
+
+    def joltage_reset(self):
+        self._joltage_reset()
 
 
 def min_button_presses(machine: Machine) -> int:
--- python/y2025/day10_2.py
+++ python/y2025/day10_2.py
@@ -1,9 +1,37 @@
+import itertools
+
 from coding_puzzle_tools import read_input
-from y2025.day10_1 import Machine
+from y2025.day10_1 import JoltageError, Machine
+
+
+def min_button_presses(machine: Machine):
+    i = 1
+    while True:
+        for combo in itertools.combinations_with_replacement(
+            range(len(machine.buttons)), i
+        ):
+            for button in combo:
+                machine.push_joltage_button(button)
+                try:
+                    machine.joltage_check()
+                except JoltageError:
+                    break
+            else:
+                if machine.joltage_check():
+                    return i
+            machine.joltage_reset()
+        i += 1
 
 
 def main(lines: list[str]) -> int:
     machines = [Machine(line) for line in lines]
+    answer = 0
+    for i, machine in enumerate(machines):
+        print(f"{i=}")
+        sub_answer = min_button_presses(machine)
+        print(f"{sub_answer=}")
+        answer += sub_answer
+    return answer
 
 
 if __name__ == "__main__":
